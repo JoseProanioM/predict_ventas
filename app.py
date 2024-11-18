fa6f0591676eaa7d7c58a5a847c73341605d0520
JoseProanioM <jose.proano.m@hotmail.com>
2024-11-18 07:40:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.graph_objects as go
 @st.cache_data
 def load_data():
     # Replace with the path to your dataset
-    data = pd.read_csv("C:/Users/josep.JOSE/OneDrive/Documents/3. Work/Oikonomics/17. OikoData/Do-Files y Scripts/streamlit_app/data/predicción_ventas.csv", parse_dates=["Periodo"])
+    data = pd.read_csv("https://raw.githubusercontent.com/JoseProanioM/predict_ventas/refs/heads/main/data/predicci%C3%B3n_ventas.csv", parse_dates=["Periodo"])
     return data
 
 # Load data
